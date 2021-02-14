d8514bcffa75f2d95c2eb47cebfa038f5224c96c
Baruc Almaguer <baruc.almaguer@gmail.com>
2021-02-14 00:50:18 -0600

    starting timers correctly =D

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,6 @@
+import os
 import sys
+from pprint import pprint, pformat
 import click
 from colorama import init as init_term, AnsiToWin32
 from termcolor import colored
@@ -50,15 +52,81 @@ pie -lf --regex "vui"
 REPORTS
 # TODO: enlist Possible report types
 '''
+home_dir = os.environ['HOME']
+cfg_dir = os.path.join(home_dir, '.pietimer')
+timers_filename = 'timers.json'
+timers_path = os.path.join(cfg_dir, timers_filename)
 
-def get_timers():
+default_config_data = {
+  'status': {
+    'last_timer_id': None
+  },
+  'timers': {}
+}
+
+def get_config():
+  '''
+    timers structure:
+    {
+      status: {
+        last_timer_id: 0
+      },
+      timers: {
+        timer1: {
+          status: 'running', # running / paused / finished
+          intervals: [
+            {
+              start_at: '<iso8601_string>',
+              end_at: None
+            }
+          ]
+        },
+        timer2: {
+          status: 'paused', # running / paused / finished
+          intervals: [
+            {
+              start_at: '<iso8601_string>',
+              end_at: '<later iso8601_string>'
+            }
+          ]
+        },
+        ...
+      }
+    }
+  '''
   import os
-  if not os.path.exists('~/.pietimer'):
+  import json
+  # validate directories information:
+  if not os.path.exists(cfg_dir):
     print(colored('No configuration directory found!! creating...', 'yellow'))
+    try:
+      os.makedirs(cfg_dir)
+    except Exception as e:
+      print(colored(f'error creating config directory: \n{e}', 'red'))
+      return None # no config dir, no timers to return
+    else:
+      print(colored('created config directory successfully', 'white'))
+  # fetch timers information:
+  if not os.path.exists(timers_path):
+    print(colored('First timer ever! yaay!', 'green'))
+    return default_config_data
+  with open(timers_path) as timers_file:
+    content = ''.join(timers_file.readlines())
+    try:
+      parsed_content = json.loads(content)
+      return parsed_content
+    except Exception as e:
+      print(colored(f'error reading configuration file! u sure is a lawful json file?\n{e}'))
+
+def init_config_file(config):
+  import json
+  serialized_config = json.dumps(config, indent=2)
+  with open(timers_path, 'w+') as config_file:
+    config_file.write(serialized_config)
 
 @click.group()
 def cli():
-    pass
+  pass
 
 # ! COMMANDS
 
@@ -67,12 +135,52 @@ def cli():
 @click.option('-n', '--name', help='name of the timer to start (task name/identifier)')
 @click.option('-kw', '--keywords', multiple=True, help='categor(y/ies) to put the timer on (ej: work, homework, etc)')
 def start(name, keywords):
+  import json
   import datetime as dt
+  import dateutil.parser
   # 1. check if timer already exists:
-  timers = get_timers()
+  config = get_config()
+  if not config:
+    print(colored('error reading config file :(\n\naborted mission', 'red'))
+    return
+  timers = config['timers']
+  if not timers:
+    init_config_file(config)
+  # 2. extract corresponding timer or create a new one:
+  if name:
+    timer_id = name
+  else:
+    # get
+    timer_id = config['status']['last_timer_id']
+    if timer_id:
+      if (status:= config['timers'][timer_id]['status']) == 'finished':
+        timer_id += 1
+    config['status']['last_timer_id'] = timer_id
+  # check if timer exists, else initialize it:
+  if not timers.get(timer_id):
+    timers[timer_id] = {'status': 'initializing', 'intervals': []}
+    print(colored('new timer created +++', 'green'))
+  if timers[timer_id]['status'] == 'finished':
+    print(colored('oops! this timer already finished, goodbye!', 'yellow'))
+    return
+  if timers[timer_id]['status'] == 'running':
+    print(colored('oops! this timer is already running! try finishing it or starting another one!', 'yellow'))
+    return
   time = dt.datetime.now()
-  print(colored(f'''started { "'" + name + "' " if name else ""}timer. Keywords: {", ".join(keywords) if keywords else ""}''', color='green'))
-  print(colored(time.isoformat(), color='green'))
+  if not timers[timer_id]['intervals']:
+    timers[timer_id]['intervals'].append({
+      'start_at': time.isoformat(),
+      'end_at': None
+    })
+  # be it an initializing or paused timer, move it to running:
+  timers[timer_id]['status'] = 'running'
+  new_config = json.dumps(config, indent=2)
+  with open(timers_path, 'r+') as config_file:
+    config_file.seek(0)
+    config_file.write(new_config)
+    config_file.truncate()
+  print(colored(f'timer started successfully! (@ {time.isoformat()})', color='green'))
+  
 
 def main():
     cli.add_command(start)
